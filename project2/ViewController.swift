9bec2117ab3bc9ed9e271f103f3097938fac0ba8
COE-02 <coe-02@coe-02s-imac.local>
2020-01-21 14:07:26 +0530

    Initial Commit

---
+++ project2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  project2
+//
+//  Created by COE-02 on 21/01/20.
+//  Copyright © 2020 COE-02. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
